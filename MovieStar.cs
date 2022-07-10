a7acebe3f151cf00c80786db6bf2ce5c4a98ca64
IvayloMollov <64088325+IvayloMollov@users.noreply.github.com>
2022-07-10 16:20:05 +0300

    Add files via upload

---
+++ MovieStar.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Csharp_Task1
+{
+    class MovieStar{
+        private string name;
+        private string gender;
+        private string nationality;
+        private int age;
+
+        public string Name{
+            get { return name; }
+            set { name = value != null ? value : ""; }
+        }
+
+        public string Gender{
+            get { return gender; }
+            set { gender = value != null ? value : "undefined"; }
+        }
+
+        public string Nationality{
+            get { return nationality; }
+            set { nationality = value != null ? value : "unknown"; }
+        }
+
+        public int Age{
+            get { return age; }
+            set { age = value > 0 ? value : 0; }
+        }
+        public MovieStar(){
+            Name = "";
+            Gender = "";
+            Nationality = "";
+            Age = 0;
+        }
+        public MovieStar(string name, 
+                         string gender, 
+                         string nationality, 
+                         int age){
+            Name = name;
+            Gender = gender;
+            Nationality = nationality;
+            Age = age;
+        }
+        public MovieStar(MovieStar other){
+            Name = other.name;
+            Gender = other.gender;
+            Nationality = other.nationality;
+            Age = other.age;
+        }
+        public override string ToString()
+        {
+            return string.Format($"Name: {Name}\n"+
+                $"Gender: {Gender}\n"+
+                $"Nationality: {Nationality}\n" +
+                $"Age: {Age} years old\n\n");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+namespace Csharp_Task1
+{
+    class Program
+    {   
+        static void Main(string[] args)
+        {
+            decimal taxation(decimal grossSalary)
+            {
+                if (grossSalary <= 1000)
+                {
+                    return grossSalary;
+                } else if (grossSalary > 1000 && grossSalary <= 3000)
+                {
+                    return (decimal)((double)(grossSalary - 1000) * 0.75 + 1000);
+                }
+                else
+                {
+                    return (decimal)((double)(grossSalary - 1000) * 0.9 + 700);
+                }
+                //the simplest way to add further taxes it via additional if() conditions 
+            }
+            Console.WriteLine(taxation(3400));
+            string fileName = @"C:\Users\Ivo\Desktop\Inensia\IOfile.txt";
+            MovieStar ivo = new MovieStar("ivo", "male", "bg", 22);
+            List<MovieStar> movieStars = new List<MovieStar> 
+            {
+                ivo,
+                new MovieStar("pesho","female","bg",29)
+            };
+            string json = JsonConvert.SerializeObject(movieStars);
+            Console.WriteLine(json+"\n");
+            if(System.IO.File.Exists(fileName) == false)
+            {
+                System.IO.File.WriteAllText(fileName, json);
+            }
+            else
+            {
+                System.IO.File.Delete(fileName);
+                System.IO.File.WriteAllText(fileName, json);
+            }
+            List<MovieStar> newStars = JsonConvert.DeserializeObject<List<MovieStar>>(json);
+            foreach (var item in newStars)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
